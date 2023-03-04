0256ed0255434d995df7d8097a720961a90e48a1
krystyna0 <krystyna3541@ukr.net>
2023-03-04 01:44:50 +0100

    add_components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import './App.css';
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+        <Header/>
+        <Main/>
+        <Footer/>
+
+        <Routes>
+            <Route path="/" element={Home} />
+        </Routes>
     </div>
   );
 }
 
 export default App;
+
